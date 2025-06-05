49505e05cafa9bb5e65a23f19c8cc857167391ea
Raphael Menges <Raphael.Menges@alfatraining.de>
2025-06-05 10:34:15 +0200

    Activate KleidiAI.

--- src/build.ts
+++ src/build.ts
@@ -129,6 +129,7 @@ await new Command()
 		}
 
 		args.push('-Donnxruntime_BUILD_UNIT_TESTS=OFF');
+		args.push('-Donnxruntime_USE_KLEIDIAI=ON');
 
 		if (compilerFlags.length > 0) {
 			const allFlags = compilerFlags.map(def => `-D${def}`).join(' ');
